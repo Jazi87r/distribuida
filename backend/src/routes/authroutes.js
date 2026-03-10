7a31591ebb529a2e4f6eb906612b41cbf89929e3
jazi87r <jazieldavidgarcia@gmail.com>
2026-03-10 18:14:56 -0500

    database stuff

--- backend/src/routes/authroutes.js
+++ backend/src/routes/authroutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import  {renderAuth}  from "../controllers/authController.js";
+import  {renderAuth, handleLogin}  from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.get("/", renderAuth);
+router.post("/login", handleLogin);
+
 
 export default router;
